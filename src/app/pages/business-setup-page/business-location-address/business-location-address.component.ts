874423c822481ed21c065109e1e6ede58a2ed9d6
DESKTOP-B7C62QE\u <vcon.joshnamj@gmail.com>
2025-02-14 17:42:21 +0530

    wertyuio

--- src/app/pages/business-setup-page/business-location-address/business-location-address.component.ts
+++ src/app/pages/business-setup-page/business-location-address/business-location-address.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'business-location-address',
   templateUrl: './business-location-address.component.html',
+  standalone: true,
   styleUrls: ['./business-location-address.component.scss']
 })
 export class BusinessLocationAddressComponent {
--- src/app/pages/business-setup-page/business-setup-page.module.ts
+++ src/app/pages/business-setup-page/business-setup-page.module.ts
@@ -3,33 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { BusinessSetupPageRoutingModule } from './business-setup-page-routing.module';
 import { BusinessSetupPageComponent } from './business-setup-page.component';
-<<<<<<< HEAD
+
 import { BusinessLocationDetailsComponent } from './business-location-details/business-location-details.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { BusinessTypeComponent } from './business-type/business-type.component';
-=======
+
 import { BusinessLocationAddressComponent } from './business-location-address/business-location-address.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import { LocationTimingsComponent } from './location-timings/location-timings.component';
 import { EditLocationComponent } from './edit-location/edit-location.component';
 import { EditBillingDetailsComponent } from './edit-billing-details/edit-billing-details.component';
->>>>>>> d5c6600c050a4485744be50afaedb10dcc68fa40
 
 
 @NgModule({
-  declarations: [
-    BusinessSetupPageComponent,
-<<<<<<< HEAD
-    BusinessLocationDetailsComponent,
-    BusinessTypeComponent
-  ],
+    declarations: [
+        BusinessSetupPageComponent,
+        BusinessLocationDetailsComponent,
+        BusinessTypeComponent,
+        EditLocationComponent
+    ],
   imports: [
     CommonModule,
     BusinessSetupPageRoutingModule,
-    ReactiveFormsModule
-  ]
-=======
+    ReactiveFormsModule,
     BusinessLocationAddressComponent,
     LocationTimingsComponent,
     EditLocationComponent,
@@ -42,6 +39,5 @@ import { EditBillingDetailsComponent } from './edit-billing-details/edit-billing
         MatCheckboxModule,
         FormsModule
     ]
->>>>>>> d5c6600c050a4485744be50afaedb10dcc68fa40
 })
 export class BusinessSetupPageModule { }
